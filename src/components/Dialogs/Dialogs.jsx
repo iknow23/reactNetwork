8f050cce9478be71c03815951321800d0fa0007c
Айнур-ПК\Айнур <felix-xilef@mail.ru>
2019-07-04 23:37:21 +0300

    Fix

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import s from './Dialogs.module.css';
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
+
+const Dialogs = (props) => {
+    let dialogsElements =  props.state.dialogs.map( d => <DialogItem name={d.name} id={d.id} />  );
+    let messagesElements = props.state.messages.map( m => <Message message={m.message}/> );
+
+    let newMessageElement = React.createRef();
+
+    let addMessage = () => {
+        let text = newMessageElement.current.value;
+        alert(text);
+    }
+
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                { dialogsElements }
+            </div>
+            <div className={s.messages}>
+                { messagesElements }
+            </div>
+            <div>
+                <textarea ref={newMessageElement}></textarea>
+            </div>
+            <button onClick={ addMessage }>Add message</button>
+        </div>
+    )
+}
+
+export default Dialogs;
